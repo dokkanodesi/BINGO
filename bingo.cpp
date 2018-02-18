0a875e829f704eab5dc43032d674e95fc360d097
dokkanodesi <35514371+dokkanodesi@users.noreply.github.com>
2018-02-18 23:48:48 +0900

    乱数がんばれ

---
+++ bingo.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main(void){
+    // Your code here!
+    int status[5][5] = {};
+    int i,j;
+    
+    printf("RAND_MAX=%d\n",RAND_MAX);
+    
+    for(i=0; i<5; i++){
+        for(j=0; j<5; j++){
+            if(i == 0){
+                //status[i][m] =rand   
+            }else if(i == 1){
+                //status[i][m]
+            }else if(i == 2){
+                //status[i][m]
+            }else if(i == 3){
+                //status[i][m]
+            }else{
+                //status[i][m]
+            }
+        }
+    }
+}
