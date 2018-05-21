7af7299cc2e97aa17f356e38eef848379e411fc4
dokkanodesi <35514371+dokkanodesi@users.noreply.github.com>
2018-05-21 19:30:41 +0900

    クソ雑魚乱数使ってビンゴカードだいぶ作れた
    そろそろgit使えるようになりませんかね…？

--- bingo.cpp
+++ bingo.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
 #include <stdio.h>
 #include <random>
+#include <time.h>
 using namespace std;
+
+int GetRandom(int min,int max);
+
 int main(void){
     int status[5][5] = {};
     int i,j;//iが横、jが縦
 	int k;
     int duplication[5] = {};
-
+	srand((unsigned int)time(NULL));
+	
     for(i=0; i<5; i++){
         for(j=0; j<5; j++){
 			Relottery:
 			
-			status[i][j] = rand() % 15 + i*15 +1;
+			status[i][j] =  GetRandom(1,15) + i*15 ;
 			duplication[j] = status[i][j];
 			//かぶったらだめ
 			//被り調査
@@ -21,7 +26,19 @@ int main(void){
 					goto Relottery;
 				}
 			}
-			printf("%d\n",status[i][j]);
         }
     }
+	//status[2][2] = 100;
+	for(i=0; i<5; i++){
+		for(j=0; j<5; j++){
+			if(j == 0 && i != 0)printf("\n");
+			printf("%d	",status[j][i]);
+		}
+	}
+}
+
+
+int GetRandom(int min,int max)
+{
+	return min + (int)(rand()*(max-min+1.0)/(1.0+RAND_MAX));
 }
