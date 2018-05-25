848ad7c1b000f2b6fac3b161b7383e1201c42167
dokkanodesi <35514371+dokkanodesi@users.noreply.github.com>
2018-05-25 19:20:19 +0900

    自作関数を進めた
    main関数の名前変えたらエラー出るからやめよう
    (ここは作業記録なのか？)
    choosecheckの返り値は考えよう
    乱数を若干改善したが、出来はまだダメ

--- bingo.cpp
+++ bingo.cpp
@@ -4,7 +4,25 @@
 #include <time.h>
 using namespace std;
 
-int GetRandom(int min,int max);
+
+int GetRandom(int min,int max)
+{
+	return min + (int)(rand()*(max-min+1.0)/(1.0+RAND_MAX));
+}
+
+int ChooseCheck(void){
+	int n = GetRandom(1,75000000) % 15 + 1;
+	int i,j;
+	int status[5][5];
+	for(i=0; i>5; i++){
+		for(j=0; j>5; j++){
+			if(status[i][j] == n){
+				status[i][j] = 0;
+			}
+		}
+	}
+	return 0;
+}
 
 int main(void){
     int status[5][5] = {};
@@ -17,7 +35,7 @@ int main(void){
         for(j=0; j<5; j++){
 			Relottery:
 			
-			status[i][j] =  GetRandom(1,15) + i*15 ;
+			status[i][j] =  GetRandom(1,1500000) % 15 + i*15 +1;
 			duplication[j] = status[i][j];
 			//かぶったらだめ
 			//被り調査
@@ -28,17 +46,16 @@ int main(void){
 			}
         }
     }
-	//status[2][2] = 100;
+	status[2][2] = 0;
 	for(i=0; i<5; i++){
 		for(j=0; j<5; j++){
 			if(j == 0 && i != 0)printf("\n");
-			printf("%d	",status[j][i]);
+			if(status[i][j]){//true、つまり開いてないなら
+				printf("%d	",status[j][i]);
+			}else{
+				printf("OPEN");
+			}
 		}
 	}
-}
-
-
-int GetRandom(int min,int max)
-{
-	return min + (int)(rand()*(max-min+1.0)/(1.0+RAND_MAX));
+	return 0;
 }
