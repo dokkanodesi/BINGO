e0fd2d1f945b9eeedbf61297d02706ea5fd76478
dokkanodesi <35514371+dokkanodesi@users.noreply.github.com>
2018-06-06 19:11:26 +0900

    数字をOPENできるようになった
    hitを消してmainの中に入れた。関数間で配列の値をすべて渡すやり方を知らん
    gitを覚えろ(n回目)

--- bingo.cpp
+++ bingo.cpp
@@ -11,22 +11,26 @@ int GetRandom(int min,int max){
 
 int Choose(void){
 	int n = GetRandom(1,75000000) % 75 + 1;
-	printf("%dです！",n);
+	//printf("%dです！",n);
 	return n;
 }
 
-int hit(){
+/*int hit(){
 	int i,j;
 	int status[5][5];
+	int num = Choose();
 	for(i=0; i>5; i++){
 		for(j=0; j>5; j++){
-			if(status[i][j] == Choose()){
+			if(num == status[i][j]){
 				status[i][j] = 0;
+				goto RoopEscape;//下5行くらいの場所に飛ぶ
 			}
 		}
 	}
-	return 0;
-}
+	RoopEscape:
+	//system("pause");
+	return status[i][j];
+}*/
 
 /*
 int bingo(){
@@ -40,7 +44,7 @@ int main(void){
     int duplication[5] = {};
 	srand((unsigned int)time(NULL));
 	
-    for(i=0; i<5; i++){
+    for(i=0; i<5; i++){//ビンゴカード作り
         for(j=0; j<5; j++){
 			Relottery:
 			status[i][j] =  GetRandom(1,1500000) % 15 + i*15 +1;
@@ -49,26 +53,33 @@ int main(void){
 			duplication[j] = status[i][j];
 			for(k=0; k<j; k++){
 				if(status[i][j] == duplication[k]){
-					goto Relottery;
+					goto Relottery;//上5行くらいに飛ぶ
 				}
 			}
         }
     }
-	status[2][2] = 0;
-	for(i=0; i<5; i++){
-		for(j=0; j<5; j++){
-			if(j == 0 && i != 0)printf("\n");
-			if(status[i][j]){//true、つまり番号が開いていないなら
-				printf("%d	",status[j][i]);
-			}else{
-				printf("OPEN	");
+	status[2][2] = 0;//中央が開く
+	
+	int cnt = 1;
+	int num;
+	while(cnt==1){//bingoしたら終わりにしたい
+		for(i=0; i<5; i++){
+			for(j=0; j<5; j++){
+				num = Choose();
+				if(num == status[i][j]){
+					status[i][j] = 0;
+				}
+				if(j == 0 && i != 0)printf("\n");//改行してカードっぽい表示にする
+				if(status[i][j]){//true、つまり番号が開いていない(0)なら
+					printf("%d	",status[j][i]);
+				}else{
+					printf("OPEN	");
+				}
 			}
 		}
+		//ビンゴ、リーチ判定
+		printf("%dです！",num);
+		cnt++;//単体だとテスト用
 	}
-	Choose();
 	return 0;
 }
-
-
-
-
