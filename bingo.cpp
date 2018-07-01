8c836dc6fb9ee24155320fb8fb46634174be9d38
dokkanodesi <35514371+dokkanodesi@users.noreply.github.com>
2018-07-01 19:13:25 +0900

    ダメ
    ビンゴの種類を記録するようにしようとしたらダメ
    実行しても動かない
    (そもそも4種じゃなく12種ビンゴのデータを集めるべきなのでは…)

--- bingo.cpp
+++ bingo.cpp
@@ -11,41 +11,18 @@ int GetRandom(int min,int max){
 
 int Choose(void){
 	int n = GetRandom(1,75000000) % 75 + 1;
-	//printf("%dです！",n);
+	////printf("%dです！",n);
 	return n;
 }
 
-/*int hit(){
-	int i,j;
-	int status[5][5];
-	int num = Choose();
-	for(i=0; i>5; i++){
-		for(j=0; j>5; j++){
-			if(num == status[i][j]){
-				status[i][j] = 0;
-				goto RoopEscape;//下5行くらいの場所に飛ぶ
-			}
-		}
-	}
-	RoopEscape:
-	//system("pause");
-	return status[i][j];
-}*/
-
-/*
-int bingo(){
-	
-}
-*/
-int main(void){
-    int status[5][5] = {};
-    int i,j;//iが横、ｊが縦
+int bingo(void){
+	int status[5][5] = {};
+	int i,j;//iが横、ｊが縦
 	int k;
-    int duplication[5] = {};
-	srand((unsigned int)time(NULL));
+	int duplication[5] = {};
 	
-    for(i=0; i<5; i++){//ビンゴカード作り
-        for(j=0; j<5; j++){
+	for(i=0; i<5; i++){//ビンゴカード作り
+		for(j=0; j<5; j++){
 			Relottery:
 			status[i][j] =  GetRandom(1,1500000) % 15 + i*15 +1;
 			
@@ -53,11 +30,11 @@ int main(void){
 			duplication[j] = status[i][j];
 			for(k=0; k<j; k++){
 				if(status[i][j] == duplication[k]){
-					goto Relottery;//上5行くらいに飛ぶ
+				goto Relottery;//上5行くらいに飛ぶ
 				}
 			}
-        }
-    }
+		}
+	}
 	status[2][2] = 0;//中央が開く
 	
 	
@@ -78,7 +55,6 @@ int main(void){
 	int Bingo_tatecnt = 0;
 	int Bingo_Centtatecnt = 0;
 	int Bingo_nanamecnt = 0;
-
 	for(i=0; i<5; i++){
 		for(j=0; j<5; j++){
 			yoko[i][j]=-1;
@@ -91,7 +67,6 @@ int main(void){
 	naname[0][2] = 0;//　＼方向
 	naname[1][2] = 0;//　／方向
 	
-
 	
 	while(cnt < 75){//bingoしたらcntが9999になる
 		Recall://下4行くらいにある
@@ -102,10 +77,9 @@ int main(void){
 			}
 		}
 		CallNumber[cnt] = num;
-		for(i=0; i<5; i++){//カードの数字が開くかどうか判定
+		for(i=0; i<5; i++){//カードの数字が開くかどうか判定、開いたら0にする
 			for(j=0; j<5; j++){
 				if(num == status[j][i]){
-					//printf("%d",tate[0][4]);
 					status[j][i] = 0;
 					OpenJudge++;
 					yoko[i][j] = 0;
@@ -116,13 +90,12 @@ int main(void){
 					if(i + j == 4){
 						naname[1][i] = 0;
 					}
-					//printf("%d",tate[0][4]);
 				}
-				if(j == 0 && i != 0)printf("\n");//改行してカードっぽい表示にする
-				if(status[j][i] != 0){//true、つまり番号が開いていない(0じゃない)なら
-					printf("%d	",status[j][i]);
-				}else{
-					printf("OPEN	");
+				if(j == 0 && i != 0)//printf("\n");//改行してカードっぽい表示にする
+				if(status[j][i] != 0){//番号が開いていないなら
+					//printf("%d	",status[j][i]);
+				}else{//開いているなら
+					//printf("OPEN	");
 				}
 			}
 		}
@@ -143,22 +116,22 @@ int main(void){
 			}
 			
 			if(yokocnt==5){
-				printf("%d列めの横ビンゴです！",i+1);
-				Bingo_yokocnt++;
+				//printf("%d列めの横ビンゴです！",i+1);
+				Bingo_yokocnt = 0;
 				goto goodbingo;//多重ループ抜け
 			}
 			if(tatecnt==5){
-				printf("%d列めの縦ビンゴです！",i+1);
+				//printf("%d列めの縦ビンゴです！",i+1);
 				if(i==2){
-					Bingo_Centtatecnt++;
+					Bingo_Centtatecnt = 1;
 				}else{
-					Bingo_tatecnt++;
+					Bingo_tatecnt = 2;
 				}
 				goto goodbingo;
 			}
 			if(nanamecnt==5){
-				printf("%d列めのななめビンゴです！",i+1);
-				Bingo_nanamecnt++;
+				//printf("%d列めのななめビンゴです！",i+1);
+				Bingo_nanamecnt = 3;
 				goto goodbingo;
 			}
 			yokocnt=0;
@@ -168,21 +141,43 @@ int main(void){
 		}
 		goodbingo:
 		//printf("%d%d%d%d\n",Bingo_yokocnt, Bingo_tatecnt, Bingo_Centtatecnt, Bingo_nanamecnt);
-		if(Bingo_yokocnt==1 || Bingo_tatecnt==1 || Bingo_Centtatecnt==1 || Bingo_nanamecnt==1){
-			printf("ビンゴだよ");
+		if(Bingo_yokocnt==0 || Bingo_Centtatecnt==1 || Bingo_tatecnt==2 || Bingo_nanamecnt==3){
+			//printf("ビンゴだよ");
 			cnt = 9999;
 		}
-		printf("%dです！",num);
+		//printf("%dです！",num);
 		if(OpenJudge == 1){
-			printf("該当する数字がオープンされます！");
+			//printf("該当する数字がオープンされます！");
 			OpenJudge--;
 		}
-		printf("\n\n");
+		//printf("\n\n");
 		cnt++;//単体だとテスト用
 	}
-	return 0;
+	if(Bingo_yokocnt == 0){
+		return Bingo_yokocnt;
+	}else if(Bingo_Centtatecnt == 1){
+		return Bingo_Centtatecnt;
+	}else if(Bingo_tatecnt == 2){
+		return Bingo_tatecnt;
+	}else if(Bingo_nanamecnt == 3){
+		return Bingo_nanamecnt;
+	}
 }
 
-
-
+int main(void){
+	srand((unsigned int)time(NULL));
+	int i,j;
+	int bingocnt[4];
+	printf("aaa");
+	for(j=0; j<4; j++){//初期化
+		bingocnt[i] = 0;
+	}
+	for(i=0; i<10; i++){//i<nのn回ビンゴ結果もってくるぞ
+		int BingoType = bingo();
+		bingocnt[BingoType]++;
+	}
+	for(i=0; i<4; i++){
+		printf("%d",bingocnt[i]);
+	}//この辺ダサすぎ
+}
 
